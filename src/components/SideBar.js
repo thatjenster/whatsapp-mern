2b88ae5697a40ecdccc1fe6198eeceb25367ef90
Jenny Dinh <jennythi.d@gmail.com>
2020-12-05 21:22:46 -0500

    adding search

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,7 +1,38 @@
+import { Avatar, IconButton } from '@material-ui/core'
+import { SearchOutlined } from '@material-ui/icons'
+import ChatIcon from '@material-ui/icons/Chat'
+import DonutLargeIcon from '@material-ui/icons/DonutLarge'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import Photo from '../asset/20160917_174959.jpg'
+
 function SideBar() {
     return (
         <div className="sidebar">
-            <h1> I am a side bar TBD</h1>
+            <div className="sidebar__header">
+                <div className="sidebar__left">
+                    <Avatar src={Photo} />
+                </div>
+                <div className="sidebar__right">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input 
+                        placeholder="Search or start new chat"
+                        type="text"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
