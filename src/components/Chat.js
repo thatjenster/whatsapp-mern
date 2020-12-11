eadff20fdc6487530859a3c170605838105d63fd
Jenny Dinh <jennythi.d@gmail.com>
2020-12-10 21:21:39 -0500

    add mui icon

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,28 @@
+const { Avatar, IconButton } = require("@material-ui/core");
+const { AttachFile, SearchOutlined, MoreVert } = require("@material-ui/icons");
+
 function Chat() {
     return (
         <div className="chat">
-            <h1> I am the CHAT TBD</h1>
+            <div className="chat__header">
+                <Avatar />
+                <div className="chat__headerInfo">
+                    <h3>ROOM NAME</h3>
+                    <p>Last seen at ...</p>
+                    <div className="chat_headerRight">
+                        <IconButton>
+                            <SearchOutlined />
+                        </IconButton>
+                        <IconButton>
+                            <AttachFile />
+                        </IconButton>
+                        <IconButton>
+                            <MoreVert />
+                        </IconButton>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
